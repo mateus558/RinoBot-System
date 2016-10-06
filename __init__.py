72329ba469fde21a7df8493f51cd606c1d1d60d7
Mateus Coutinho Marim <mateus.marim@ice.ufjf.br>
2016-10-06 00:32:06 -0300

    teste

--- __init__.py
+++ __init__.py
@@ -8,13 +8,13 @@ if not eye.open_camera(0):
     sys.exit()
     print "done"
 
-eye.init_record()
+#eye.init_record()
 i = 0
 while eye.get_new_frame():
     if i == 100:
         break
     eye.adjust_gamma(0.7)
-    #eye.to_hsv()
+    eye.to_hsv()
     #eye.to_gray()
     #eye.equalize_histogram()
     eye.gauss_blur()
@@ -23,8 +23,8 @@ while eye.get_new_frame():
     eye.show_processed_vision()
     #eye.show("bilateral", frame)
     eye.show()
-    eye.save_image("teste.png", eye.get_raw_frame())
-    eye.record()
+    #eye.save_image("teste.png", eye.get_raw_frame())
+    #eye.record()
     i += 1
 print "done"
 
