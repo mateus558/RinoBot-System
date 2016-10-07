ab525f1507f51b1eb53f21e21b5ae3c61ce23b68
Mateus Coutinho Marim <mateus.marim@ice.ufjf.br>
2016-10-06 22:13:03 -0300

    Some changes

--- test_vision.py
+++ test_vision.py
@@ -7,22 +7,14 @@ if not eye.open_camera(0):
     sys.exit()
     print "done"
 
-#eye.init_record()
+eye.init_record()
 i = 0
+
 while eye.get_new_frame():
-    if i == 100:
+    if i == 100000:
         break
-    eye.adjust_gamma(0.7)
-    eye.to_hsv()
-    #eye.to_gray()
-    #eye.equalize_histogram()
-    eye.gauss_blur()
-    #frame = eye.get_raw_frame()
-    #frame = eye.bilateral_blur(frame)
     eye.show_processed_vision()
-    #eye.show("bilateral", frame)
     eye.show()
-    #eye.save_image("teste.png", eye.get_raw_frame())
-    #eye.record()
     i += 1
+
 print "done"
